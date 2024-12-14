196dff0141566d8f9457e2ac5566bc3500d8ac5c
Kavindu Kaveesha <140581819+kavindukaveesha@users.noreply.github.com>
2024-12-14 11:01:54 +0530

    Implement Application auditing Autowire config

---
+++ API/src/main/java/com/NextCoreInv/book_network/confg/ApplicationAuditAware.java
@@ -0,0 +1,25 @@
+package com.NextCoreInv.book_network.confg;
+
+import com.NextCoreInv.book_network.user.User;
+import org.springframework.context.ApplicationListener;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Optional;
+
+public class ApplicationAuditAware implements AuditorAware<Integer> {
+    @Override
+    public Optional<Integer> getCurrentAuditor() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+         if(authentication ==null ||
+            !authentication.isAuthenticated() ||
+                 authentication instanceof AnonymousAuthenticationToken){
+             return Optional.empty();
+         }
+         User userprincipal = (User) authentication.getPrincipal();
+
+        return Optional.ofNullable(userprincipal.getId());
+    }
+}
